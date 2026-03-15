6d66a335a83e5d5a5c0030767651102176556edd
zhangxijing97 <zhangxijing97@gmail.com>
2026-03-15 13:42:26 -0700

    Update

---
+++ midterm/script.js
@@ -0,0 +1,51 @@
+// const list = document.getElementById("todos");
+
+// const todos = [
+//   "get groceries",
+//   "go to the bank",
+//   "run for president",
+//   "do laundry"
+// ];
+
+// for (let i = 0; i < todos.length; i++) {
+//   const li = document.createElement("li");
+//   li.textContent = todos[i];
+//   console.log(li.textContent);
+//   list.appendChild(li);
+// }
+
+const arr = [1, 2, 3];
+
+// arr.shift();
+// console.log(arr);
+
+// arr.pop();
+// console.log(arr);
+
+// console.log(arr.pop());
+
+// console.log(arr[arr.length - 1])
+
+console.log(arr[arr.length])
+
+// const arr = [];
+// for(let i = 0; i <= 10; i = i + 2) {
+//    arr.push(i);
+// }
+// console.log(arr);
+
+// const paragraphNode = document.createElement('p');
+// const text = document.createTextNode('This is dynamic content');
+// paragraphNode.appendChild(text);
+// const parent = document.getElement('app');
+// parent.appendChild(paragraphNode);
+
+// const paragraphNode = document.createElement('p');
+// const text = document.createTextNode('This is dynamic content');
+// paragraphNode.appendChild(text);
+// const parent = document.getElementById('app');
+// parent.appendChild(paragraphNode);
+
+// const a = '10.4';
+// const b = parseInt(a);
+// console.log(b);
